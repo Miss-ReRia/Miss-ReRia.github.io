df980883ef23fdaa63c63f6ca31f1f72019c6836
Miss-ReRia <707823814@qq.com>
2021-07-24 13:30:50 +0800

    Site updated: 2021-07-24 13:30:49

---
+++ js/FunnyTitle.js
@@ -0,0 +1,17 @@
+// 浏览器搞笑标题
+var OriginTitle = document.title;
+var titleTime;
+document.addEventListener('visibilitychange', function () {
+    if (document.hidden) {
+        // $('[rel="icon"]').attr('href', "/funny.ico");
+        document.title = '(´ฅωฅ‘) 藏起来啦~';
+        clearTimeout(titleTime);
+    }
+    else {
+        // $('[rel="icon"]').attr('href', "/favicon.ico");
+        document.title = 'Σ(っ °Д °;)っ 被发现了' + OriginTitle;
+        titleTime = setTimeout(function () {
+            document.title = OriginTitle;
+        }, 2000);
+    }
+});
